e2ba671b20d3f582a239de4ca705aaef58754718
Goldie <goldiejackson25@yahoo.com>
2021-09-10 07:20:34 -0500

    commit tree 1dff8d2891a12843c1a1a64ac4516229d5bd2022

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+
+function submitData(userName, userEmail){
+    
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type' : 'application/json',
+            'Accept' : 'application/json'
+        },
+        body: JSON.stringify(
+            {
+            name: userName,
+            email: userEmail,
+        }),
+    })
+        .then(res => res.json())
+        .then(data => document.querySelector('body').append(data.id))
+        .catch(function(error){
+            document.querySelector('body').append(error.message)
+        })
+    
+};
+
